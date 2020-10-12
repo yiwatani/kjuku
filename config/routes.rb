ad37ecfa1488290e3ef1b0f6665ad021214c29bf
yiwatani <dbhammer17@gmail.com>
2020-10-12 15:22:09 +0900

    scrollifyの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  resources :homes, only: :index
+  resources :homes, only: :index do
+    collection do
+      get :about 
+    end
+  end
   resources :menus, only: :index
 end
